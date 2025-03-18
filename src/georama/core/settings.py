2bb42e42396303c25f662d871b1fe25d3a2dee65
Clemens Rudert <crud3_rt@posteo.ch>
2025-03-18 18:15:11 +0100

    fix pre commit

--- src/georama/core/settings.py
+++ src/georama/core/settings.py
@@ -36,6 +36,9 @@ GEORAMA_ALLOWED_HOSTS = os.environ.get("GEORAMA_ALLOWED_HOSTS", "").split(" ")
 
 ALLOWED_HOSTS = [] + GEORAMA_ALLOWED_HOSTS
 
+if bool(os.environ.get("GEORAMA_SECURE_PROXY_SSL_HEADER", False)):
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 # Application definition
 
 INSTALLED_APPS = [
