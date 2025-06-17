ae1cd5b758b3cf6ed5bbd2ddb679865db802fb1d
Clemens Rudert <crud3_rt@posteo.ch>
2025-06-17 14:28:01 +0200

    sitisfy suggestions

--- src/georama/maps/apps.py
+++ src/georama/maps/apps.py
@@ -4,4 +4,4 @@ from georama.core.apps import GeoramaAbstractConfig
 class MapsConfig(GeoramaAbstractConfig):
     default_auto_field = "django.db.models.BigAutoField"
     verbose_name = "Maps"
-    name = f"georama.maps"
+    name = "georama.maps"
--- src/georama/webgis/apps.py
+++ src/georama/webgis/apps.py
@@ -4,4 +4,4 @@ from georama.core.apps import GeoramaAbstractConfig
 class WebgisConfig(GeoramaAbstractConfig):
     default_auto_field = "django.db.models.BigAutoField"
     verbose_name = "Webgis"
-    name = f"georama.webgis"
+    name = "georama.webgis"
