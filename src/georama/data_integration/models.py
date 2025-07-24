9b5307cb3ab36d3740af20ad9aa13d8c605a7f71
Pi Moll <patricia@opengis.ch>
2025-07-24 16:15:34 +0200

    Add field description to model

--- src/georama/data_integration/models.py
+++ src/georama/data_integration/models.py
@@ -217,6 +217,7 @@ class Field(models.Model):
     type_oapif = models.CharField(null=False, default="UNSET", max_length=1000)
     type_oapif_format = models.CharField(null=True, default="UNSET", max_length=1000)
     alias = models.CharField(null=False, default="UNSET", max_length=1000)
+    comment = models.CharField(null=False, default="UNSET", max_length=1000)
     nullable = models.BooleanField(null=False, default=True)
     length = models.IntegerField(null=True)
     precision = models.IntegerField(null=True)
@@ -238,6 +239,7 @@ class Field(models.Model):
             type_oapif=self.type_oapif,
             type_oapif_format=self.type_oapif_format,
             alias=self.alias,
+            comment=self.comment,
             nullable=self.nullable,
             length=self.length,
             precision=self.precision,
