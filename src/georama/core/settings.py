e378e5b4d0e1b8e986a085dd6cc4dd975b31404d
Lukas Graf <lukas@opengis.ch>
2025-07-29 17:08:08 +0200

    Change default for DATA_INTEGRATION_ROOT to test projects.

--- src/georama/core/settings.py
+++ src/georama/core/settings.py
@@ -208,7 +208,7 @@ class Base(Configuration):
 
     QSL_REDIS_URL = values.Value("redis://localhost:1234", environ_prefix="")
     JOB_TIMEOUT = values.FloatValue(1000, environ_prefix="GEORAMA")
-    DATA_INTEGRATION_ROOT = values.Value("/io/data", environ_prefix="GEORAMA")
+    DATA_INTEGRATION_ROOT = values.Value("./tests/resources/projects", environ_prefix="GEORAMA")
 
     # Password validation
     # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
