54732e9afa89c91ae14e22398ea012b6772b013c
Lukas Graf <lukas@opengis.ch>
2025-07-29 14:40:52 +0200

    features app: Don't read settings directly from environ

--- src/georama/features/features_config.py
+++ src/georama/features/features_config.py
@@ -1,10 +1,10 @@
-import os
+from django.conf import settings
 
 
 class Config:
     @property
     def path(self) -> str:
-        return os.path.join(os.environ.get("GEORAMA_DATA_INTEGRATION_ROOT", "/io/data"))
+        return settings.DATA_INTEGRATION_ROOT
 
     @property
     def default_crs(self):
