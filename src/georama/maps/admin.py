e191cc94367783fe5720823ca25114824b9737eb
Pi Moll <patricia@opengis.ch>
2025-08-11 15:26:25 +0200

    Open up form when clicking on layer icon, name or title in overview

--- src/georama/maps/admin.py
+++ src/georama/maps/admin.py
@@ -47,6 +47,7 @@ class PublishedAsWmsAdmin(admin.ModelAdmin):
         "preview_image",
     ]
     list_editable = ["public"]
+    list_display_links = ["icon_column", "name", "title"]
     add_form_template = "admin/maps/publishedaswms/publish.html"
     readonly_fields = ["dataset_detail"]
     list_filter = ["name", "title"]
