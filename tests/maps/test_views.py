0dd71a3b9856ab0408beffe5e5ed5500723f5507
Clemens Rudert <crud3_rt@posteo.ch>
2025-08-13 11:55:25 +0200

    fix tests so that they are using mock instead of real end to end chain

--- tests/maps/test_views.py
+++ tests/maps/test_views.py
@@ -1,81 +1,117 @@
+from io import BytesIO
+from unittest.mock import AsyncMock, patch
+
 import pytest
 from lxml import etree
+from PIL import Image
+from qgis_server_light.interface.job import JobResult
 
 from georama.maps.models import PublishedAsWms
 
 pytestmark = pytest.mark.django_db
 
 
+def empty_png_bytes(width: int, height: int) -> JobResult:
+    """
+
+    Args:
+        width: in pixels
+        height: in pixels
+
+    Returns:
+        The image buffer
+    """
+    img = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+
+    return JobResult(
+        data=buf.getvalue(),
+        content_type="image/png",
+    )
+
+
 class TestMapsViews:
     def test_publish_as_wms_view(
         self, client, integrated_project, admin_user_name, admin_password
     ):
-        client.login(username=admin_user_name, password=admin_password)
-        vector_dataset = integrated_project.vector_datasets.get(title="TestPointLayer")
+        mock_instance = AsyncMock()
+        mock_instance.post.return_value = empty_png_bytes(32, 32)
 
-        # Publish layer as WMS
-        response = client.get(
-            f"/maps/publish_as/wms/vector/{vector_dataset.id}",
-            follow=True,
-        )
+        with (
+            patch(
+                "qgis_server_light.interface.dispatcher.RedisQueue.create",
+                new_callable=AsyncMock,
+            ) as mock_create
+        ):
+            mock_create.return_value = mock_instance
 
-        assert response.status_code == 200
-        assert b"TestPointLayer" in response.content
-        assert PublishedAsWms.objects.filter(title="TestPointLayer").exists()
+            client.login(username=admin_user_name, password=admin_password)
+            vector_dataset = integrated_project.vector_datasets.get(title="TestPointLayer")
 
-        # Get WMS Capabilities document
-        url = "/maps?SERVICE=WMS&REQUEST=GETCAPABILITIES&VERSION=1.3.0"
-        response = client.get(url)
+            # Publish layer as WMS
+            response = client.get(
+                f"/maps/publish_as/wms/vector/{vector_dataset.id}",
+                follow=True,
+            )
 
-        assert response.status_code == 200
-        assert response["Content-Type"] == "text/xml"
+            assert response.status_code == 200
+            assert b"TestPointLayer" in response.content
+            assert PublishedAsWms.objects.filter(title="TestPointLayer").exists()
 
-        parser = etree.XMLParser(ns_clean=True)
-        root = etree.fromstring(response.content, parser)
+            # Get WMS Capabilities document
+            url = "/maps?SERVICE=WMS&REQUEST=GETCAPABILITIES&VERSION=1.3.0"
+            response = client.get(url)
 
-        NS = {
-            "w": "http://www.opengis.net/wms",
-            "x": "http://www.w3.org/1999/xlink",
-        }
+            assert response.status_code == 200
+            assert response["Content-Type"] == "text/xml"
 
-        def xp(el, path):
-            nodes = el.xpath(path, namespaces=NS)
-            if isinstance(nodes, list) and len(nodes) == 1:
-                return nodes[0]
-            return nodes
+            parser = etree.XMLParser(ns_clean=True)
+            root = etree.fromstring(response.content, parser)
 
-        assert root.tag == "{http://www.opengis.net/wms}WMS_Capabilities"
-        assert root.attrib["version"] == "1.3.0"
+            NS = {
+                "w": "http://www.opengis.net/wms",
+                "x": "http://www.w3.org/1999/xlink",
+            }
 
-        # Service
-        service = xp(root, "./w:Service")
-        title = xp(service, "./w:Title/text()")
-        abstract = xp(service, "./w:Abstract/text()")
-        assert title == "QGIS Server light"
-        assert abstract == "this is the new approach"
+            def xp(el, path):
+                nodes = el.xpath(path, namespaces=NS)
+                if isinstance(nodes, list) and len(nodes) == 1:
+                    return nodes[0]
+                return nodes
 
-        # Capability
-        capability = xp(root, "./w:Capability")
+            assert root.tag == "{http://www.opengis.net/wms}WMS_Capabilities"
+            assert root.attrib["version"] == "1.3.0"
 
-        request = xp(capability, "./w:Request")
-        get_capabilities = xp(request, "./w:GetCapabilities")
-        formats = xp(get_capabilities, "./w:Format/text()")
-        assert formats == ["text/xml", "application/json"]
+            # Service
+            service = xp(root, "./w:Service")
+            title = xp(service, "./w:Title/text()")
+            abstract = xp(service, "./w:Abstract/text()")
+            assert title == "QGIS Server light"
+            assert abstract == "this is the new approach"
 
-        http_get = xp(get_capabilities, "./w:DCPType/w:HTTP/w:Get")
-        href = xp(http_get, "./w:OnlineResource/@x:href")
-        assert href == "http://testserver/maps?"
+            # Capability
+            capability = xp(root, "./w:Capability")
 
-        # Layers
-        layers = xp(capability, "./w:Layer")
-        name = xp(layers, "./w:Name/text()")
-        title = xp(layers, "./w:Title/text()")
-        abstract = xp(layers, "./w:Abstract/text()")
+            request = xp(capability, "./w:Request")
+            get_capabilities = xp(request, "./w:GetCapabilities")
+            formats = xp(get_capabilities, "./w:Format/text()")
+            assert formats == ["text/xml", "application/json"]
 
-        assert name == "qgis_server_light"
-        assert title == "QGIS Server light"
-        assert abstract == "The lightning fast access to your raster data"
+            http_get = xp(get_capabilities, "./w:DCPType/w:HTTP/w:Get")
+            href = xp(http_get, "./w:OnlineResource/@x:href")
+            assert href == "http://testserver/maps?"
 
-        layer = xp(layers, "./w:Layer")
-        assert xp(layer, "./w:Title/text()") == "TestPointLayer"
-        assert xp(layer, "./w:CRS/text()") == ["EPSG:4326", "CRS:84"]
+            # Layers
+            layers = xp(capability, "./w:Layer")
+            name = xp(layers, "./w:Name/text()")
+            title = xp(layers, "./w:Title/text()")
+            abstract = xp(layers, "./w:Abstract/text()")
+
+            assert name == "qgis_server_light"
+            assert title == "QGIS Server light"
+            assert abstract == "The lightning fast access to your raster data"
+
+            layer = xp(layers, "./w:Layer")
+            assert xp(layer, "./w:Title/text()") == "TestPointLayer"
+            assert xp(layer, "./w:CRS/text()") == ["EPSG:4326", "CRS:84"]
